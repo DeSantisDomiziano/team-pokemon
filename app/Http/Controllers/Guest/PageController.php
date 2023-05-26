4d9034079d7fc9956193c51c6eb6a0b3a5fb579c
francescominutella <eso.fra24@gmail.com>
2023-05-26 16:49:48 +0200

    add PC and Model

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+use App\Models\Pokemon;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        $pokemon = Pokemon::all();
+        return view("home", compact("pokemon"));
+    }
+
+    
+
+
+}
