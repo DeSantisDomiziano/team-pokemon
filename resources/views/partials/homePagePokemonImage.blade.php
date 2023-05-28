8ac0f74441bb1c7db63659439bed48e1488a3db5
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-28 17:36:56 +0200

    ADD higher resolution imgs but still small in size

--- resources/views/partials/homePagePokemonImage.blade.php
+++ resources/views/partials/homePagePokemonImage.blade.php
@@ -1,1 +1,1 @@
-<img width="50%" src="{{ Vite::asset('resources/pokemon/' . $pokemon[0]->id . '.png') }}" alt="{{ $pokemon[0]->name . ' image' }}">
+<img width="50%" src="{{ Vite::asset('resources/img/pokemon/' . $pokemon[0]->id . '.png') }}" alt="{{ $pokemon[0]->name . ' image' }}">
--- resources/views/partials/pokemonPagePokemonImage.blade.php
+++ resources/views/partials/pokemonPagePokemonImage.blade.php
@@ -1,1 +1,1 @@
-<img width="50%" src="{{ Vite::asset('resources/pokemon/' . $singlePokemon[0]->id . '.png') }}" alt="{{ $singlePokemon[0]->name . ' image' }}">
+<img width="50%" src="{{ Vite::asset('resources/img/pokemon/' . $singlePokemon[0]->id . '.png') }}" alt="{{ $singlePokemon[0]->name . ' image' }}">
