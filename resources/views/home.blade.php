62a512fbf4baea6f71f06378a3445b1fb1a94063
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-28 15:05:51 +0200

    FIX main section height

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app')
 @section('content')
 <div class="mainpokedex">
-    <div class="container">
-        <div class="row py-2">
-            <div class="col-6">
-                <div class="titlepokemon">
-                    <h2 class="text-center m-0 py-2 text-uppercase">{{$pokemon[0]->name}}</h2>
-                </div>
-                <div class="img_pokemon text-center">
-                    <img width="50%" src="{{ Vite::asset('resources/pokemon/' . $pokemon[0]->id . '.png') }}" alt="{{ $pokemon[0]->name . ' image'}}">
+    <div class="container h-100">
+        <div class="row py-2 h-100">
+            <div class="col-6 d-flex flex-column justify-content-between">
+                <div class="pokedex_left_top_section">
+                    <div class="titlepokemon">
+                        <h2 class="text-center m-0 py-2 text-uppercase">{{$pokemon[0]->name}}</h2>
+                    </div>
+                    <div class="img_pokemon text-center">
+                        <img width="50%" src="{{ Vite::asset('resources/pokemon/' . $pokemon[0]->id . '.png') }}" alt="{{ $pokemon[0]->name . ' image'}}">
+                    </div>
                 </div>
                 <div class="check_pokemon">
                     <?php $catched = rand(0, 100); ?>
