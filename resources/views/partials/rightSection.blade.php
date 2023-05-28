9f2598ead0b91f4477bc846caaef69e8044cd55b
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-05-28 19:46:57 +0200

    FIX style details

--- resources/views/partials/rightSection.blade.php
+++ resources/views/partials/rightSection.blade.php
@@ -1,7 +1,7 @@
 <div class="col-6 h-100">
   <ul class="list_pokemon p-0 m-0 list-unstyled">
       @foreach($pokemon as $index => $singlepokemon)
-      <li class="mb-1 {{ $index + 1 === $selectedPokemon[0] -> id ? 'active' : '' }}">
+      <li class="{{ $index + 1 === $selectedPokemon[0] -> id ? 'active' : '' }}">
           <a class="text-dark d-flex justify-content-between align-items-center text-decoration-none" href="{{ route('selectedPokemon', $index) }}">
               <p class="number m-0">{{ $index + 1 }}</p>
               <img height="50" src="{{ Vite::asset('resources/img/pokemon/'. $index + 1 .'.png') }}" alt="{{ $singlepokemon->name . ' image' }}">
