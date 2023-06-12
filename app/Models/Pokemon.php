016dde016ba7858f0b95ae686bd0cece32fdd4fa
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-12 15:59:58 +0200

    ADD belongsToMany relationship

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Pokemon extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'type_1', 'type_2', 'total', 'hp', 'attack', 'defense', 'sp_atk', 'sp_def', 'speed', 'generation', 'legendary'];
-}
 
+    public function types(): BelongsToMany
+    {
+        return $this->belongsToMany(Type::class);
+    }
+}
