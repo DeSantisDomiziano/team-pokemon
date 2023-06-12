6ac18dff49e64cf379b69cdf3db6cce1aa8ecd1c
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-12 15:59:28 +0200

    ADD beautiful types array by Lollo

---
+++ config/db.php
@@ -0,0 +1,26 @@
+<?php
+  
+  return [
+    "types" => [
+    "Fuoco",
+    "Acqua",
+    "Erba",
+    "Elettro",
+    "Volante",
+    "Veleno",
+    "Psico",
+    "Normale",
+    "Roccia",
+    "Terra",
+    "Lotta",
+    "Coleottero",
+    "Spettro",
+    "Drago",
+    "Acciaio",
+    "Buio",
+    "Ghiaccio",
+    "Folletto"
+    ],
+  ];
+
+?>
