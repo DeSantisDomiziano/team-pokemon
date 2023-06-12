be4bad1a7859dbe7a83a8f66af8a4a5cad424f6e
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-06-12 16:54:02 +0200

    ADD color to types

--- config/db.php
+++ config/db.php
@@ -2,25 +2,24 @@
   
   return [
     "types" => [
-    "Fuoco",
-    "Acqua",
-    "Erba",
-    "Elettro",
-    "Volante",
-    "Veleno",
-    "Psico",
-    "Normale",
-    "Roccia",
-    "Terra",
-    "Lotta",
-    "Coleottero",
-    "Spettro",
-    "Drago",
-    "Acciaio",
-    "Buio",
-    "Ghiaccio",
-    "Folletto"
+      ["Fuoco", "#FF4500"],
+      ["Acqua", "#1E90FF"],
+      ["Erba", "#00FF00"],
+      ["Elettro", "#FFD700"],
+      ["Volante", "#B0E0E6"],
+      ["Veleno", "#800080"],
+      ["Psico", "#FF69B4"],
+      ["Normale", "#A9A9A9"],
+      ["Roccia", "#D2B48C"],
+      ["Terra", "#DEB887"],
+      ["Lotta", "#CD5C5C"],
+      ["Coleottero", "#6495ED"],
+      ["Spettro", "#4B0082"],
+      ["Drago", "#9932CC"],
+      ["Acciaio", "#708090"],
+      ["Buio", "#2F4F4F"],
+      ["Ghiaccio", "#00FFFF"],
+      ["Folletto", "#FFB6C1"]
     ],
   ];
-
 ?>
